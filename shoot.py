df50aac13819630ea3208f9b56a704d5e6a30573
bhbmaster <bhbmaster@gmail.com>
2020-11-15 23:59:58 -0800

    first commit - can shoot ball with mouse down and up - v0.0.1

---
+++ shoot.py
@@ -0,0 +1,117 @@
+import pygame
+import pygame.locals
+import sys
+import random
+import math
+
+version = "0.0.1"
+X = 1000
+Y = 1000
+FPS = 30
+DIAGONAL = math.sqrt(X**2 + Y**2)
+
+pygame.init()
+clock = pygame.time.Clock()
+surface = pygame.display.set_mode((X,Y))
+pygame.display.set_caption(f"Shoot Ball v{version}")
+
+grey = pygame.Color(50,50,50)
+yellow = pygame.Color(200,200,0)
+red = pygame.Color(200, 20, 0)
+
+font = pygame.font.Font("FiraCode-Bold.ttf", 12)
+msg = "Shoot the ball by clicking on the screen"
+
+circle_exists = False
+circle_time_mover = 0
+circle_min_speed = 0.5
+circle_max_speed = 30
+circle_speed = 0
+circle_fire_degrees = 0
+circle_radius = 50
+circle_shades = 20
+circle_mouse_pos = []
+
+while True:
+    surface.fill(grey)
+    if circle_exists:
+        circle_time_mover += circle_speed
+    # events
+    events = pygame.event.get()
+    for ev in events:
+        if ev.type == pygame.locals.QUIT or (ev.type == pygame.locals.KEYUP and ev.key == pygame.locals.K_q):
+            # quit game if press X or hit q key
+            pygame.display.quit()
+            sys.exit()
+        if ev.type == pygame.locals.MOUSEBUTTONDOWN:
+            x_mouse , y_mouse = ev.pos
+            circle_mouse_pos.append((x_mouse , y_mouse)) # note: doesn't have to be list (only 1 item saved with each mousedown and mouseup set)
+            # circle_exists = True
+            # circle_time_mover = 0
+            #mx = random.choice([-1,0,1])
+            #my = random.choice([-1,0,1])
+            # mx = math.cos(circle_fire_degrees*math.pi/180)
+            # my = -math.sin(circle_fire_degrees*math.pi/180)
+        if ev.type == pygame.locals.MOUSEBUTTONUP:
+            x_mouse , y_mouse = ev.pos
+            x0=circle_mouse_pos[0][0]
+            y0=circle_mouse_pos[0][1]
+            x1=x_mouse
+            y1=y_mouse
+            dx=(x1-x0)
+            dy=(y1-y0)
+            # direction
+            theta_radians = math.atan2(dy,dx)
+            theta_degrees = theta_radians * 180 / math.pi
+            adjusted_degress = theta_degrees*-1
+            # adjusted_degress = adjusted_degress+180  # uncomment to shoot like pool
+            cDx = math.cos(adjusted_degress*math.pi/180)
+            cDy = -math.sin(adjusted_degress*math.pi/180)
+            # speed
+            arrow_length = math.sqrt(dx**2 + dy**2)
+            max_speed_arrow_length = DIAGONAL/2
+            circle_speed = arrow_length / (max_speed_arrow_length/circle_max_speed)  # dividing by 40 makes it so max speed is 30
+            if circle_speed > circle_max_speed: circle_speed=circle_max_speed
+            if circle_speed < circle_min_speed: circle_speed=circle_min_speed
+            # setting circle
+            circle_exists = True
+            msg = f"DEBUG: ({x0},{y0}) @ {int(adjusted_degress)}deg | speed={int(circle_speed)}"
+            pygame.draw.circle(surface, pygame.Color(0,0,255), (x0, y0), 5, 0) # red start
+            pygame.draw.circle(surface, pygame.Color(255,0,0), (x1, y1), 5, 0) # green end
+            circle_mouse_pos = [] # reset mouse events
+            circle_time_mover = 0 # reset mover
+
+
+    # drawings
+    if circle_exists:
+        circle_shades_prime = circle_shades + 1
+        for i in range(circle_shades_prime):
+            ratioR = (circle_shades_prime - i) / circle_shades_prime # moves from 1 to 0 smoothly (aka Ratio Reverse)
+            ratio = 1-ratioR                                         # moves from 0 to 1 smoothly
+            cRed = 200
+            cGreenVar = 200
+            cGreen = int(cGreenVar*ratio) + (255-cGreenVar)
+            cBlue = 0
+            cColor = pygame.Color(cRed,cGreen, cBlue)
+            cX = x0+cDx*circle_time_mover
+            cY = y0+cDy*circle_time_mover
+            cR = circle_radius*ratio # moves from radius 0 to 1*circle_radius (small to big)
+            cW = int(circle_radius/circle_shades_prime)+1
+            pygame.draw.circle(surface, cColor, (cX, cY), cR, cW)
+            
+    # msg - make into class
+    msg_surface = font.render(msg, True, red)
+    msg_rect = msg_surface.get_rect()
+    msg_rect.topleft = (90,100)
+    surface.blit(msg_surface,msg_rect)
+    # screen update
+    pygame.display.update()
+    clock.tick(FPS)
+
+# EOF
+
+
+
+
+
+
